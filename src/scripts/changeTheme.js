5211bc05c6eb5c430a92fd32b2b4f838cdc6412a
Md. Asad Chowdhary <mythasad@gmail.com>
2024-02-12 02:06:26 +0600

    Text formating.

--- src/scripts/changeTheme.js
+++ src/scripts/changeTheme.js
@@ -1,15 +1,15 @@
-function changeThemeColor(){
+function changeThemeColor() {
     var dayMode = document.getElementById("dayMode");
     var nightMode = document.getElementById("nightMode");
     var body = document.body;
-    if (dayMode.style.display=="none"){
-        dayMode.style.display="inline-block";
-        nightMode.style.display="none";
-        body.style.backgroundColor="#cbeec3";
+    if (dayMode.style.display == "none") {
+        dayMode.style.display = "inline-block";
+        nightMode.style.display = "none";
+        body.style.backgroundColor = "#cbeec3";
     }
     else {
-        dayMode.style.display="none";
-        nightMode.style.display="inline-block";
-        body.style.backgroundColor="#1f1f1f";
+        dayMode.style.display = "none";
+        nightMode.style.display = "inline-block";
+        body.style.backgroundColor = "#1f1f1f";
     }
 }
