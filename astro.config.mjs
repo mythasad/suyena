7ba25f0fcc3303df8260f505028c3d541bf27470
Md. Asad Chowdhary <mythasad@gmail.com>
2024-03-26 15:12:23 +0600

    Package Modification

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
+import db from "@astrojs/db";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://suyena.com',
-  integrations: [sitemap()]
+  integrations: [sitemap(), db()]
 });
